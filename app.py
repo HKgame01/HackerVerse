45404cd339227b05b8eaaabf01d4fcf7e2ea2d80
A5TA <cardillojamesbrandon@gmail.com>
2022-05-07 23:50:24 -0400

    changed file name

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Decorator allows us to navigate the website
+@app.route('/')
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
