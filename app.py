349f645b3f354050b01faf22b46ea9a4e22f84aa
A5TA <cardillojamesbrandon@gmail.com>
2022-05-08 01:56:55 -0400

    Improved home page and added AR hyperlinks from echo3d

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+#AR Models are found on sketchfab.com
 @app.route('/store')
 def store_page():
     items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500, 'AR':''},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900, 'AR':''},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150, 'AR': ''}
+        {'id': 1, 'name': 'Phone', 'barcode': '99944432', 'price': 500, 'AR':'https://go.echo3d.co/31qu'},
+        {'id': 2, 'name': 'Laptop', 'barcode': '34059943', 'price': 900, 'AR':'https://go.echo3d.co/sZES'},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '23143512', 'price': 150, 'AR': 'https://go.echo3d.co/cgi7'},
+        {'id': 4, 'name': 'Microphone', 'barcode': '23455324', 'price': 50, 'AR': 'https://go.echo3d.co/ZB6b'}
     ]
     return render_template('store.html', items=items)
 
