b7cfc37d5a6e1875a760da9ae46f0d0885494dea
A5TA <cardillojamesbrandon@gmail.com>
2022-05-08 00:23:23 -0400

    Built store and home pages

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route('/store')
+def store_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500, 'AR':''},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900, 'AR':''},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150, 'AR': ''}
+    ]
+    return render_template('store.html', items=items)
+
 if __name__ == "__main__":
     app.run(debug=True)
